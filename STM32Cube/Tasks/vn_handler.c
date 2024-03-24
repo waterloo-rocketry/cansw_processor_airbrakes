04a618895b2c851860a3ffb7fd0688d473a2da82
Joe <joedolina@gmail.com>
2024-03-24 09:21:37 -0400

    Add and link Vectornav common C/python libraries

---
+++ STM32Cube/Tasks/vn_handler.c
@@ -0,0 +1,14 @@
+/*
+ * vn_handler.c
+ *
+ *  Created on: Mar 24, 2024
+ *      Author: joedo
+ */
+
+
+#include "vn_handler.h"
+
+xQueueHandle rawIMUQueue;
+xQueueHandle vnStateQueue;
+
+
---
+++ STM32Cube/Tasks/vn_handler.h
@@ -0,0 +1,43 @@
+/*
+ * vn_handler.h
+ *
+ *  Created on: Mar 24, 2024
+ *      Author: joedo
+ */
+
+#ifndef VN_HANDLER_H_
+#define VN_HANDLER_H_
+
+#include "stm32h7xx_hal.h"
+#include "freertos.h"
+
+extern UART_HandleTypeDef huart1;
+extern TIM_HandleTypeDef htim1;
+
+typedef struct {
+	uint32 timestamp;
+	float accel_x;
+	float accel_y;
+	float accel_z;
+	float gyro_x;
+	float gyro_y;
+	float gyro_z;
+	float mag_x;
+	float mag_y;
+	float mag_z;
+} vn_imu_data_t;
+
+typedef struct {
+	uint32 timestamp;
+	float gps_lat;
+	float gps_lon;
+	float gps_alt;
+} vn_gps_data_t;
+
+typedef struct {
+
+} vn_full_state_t;
+
+
+
+#endif /* VN_HANDLER_H_ */
