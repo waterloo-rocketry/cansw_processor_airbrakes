b25933e70da01f4814a5e3c595189d18b4fce675
Joe <joedolina@gmail.com>
2024-04-14 12:21:18 -0400

    move controller params to struct

--- STM32Cube/Tasks/controller.c
+++ STM32Cube/Tasks/controller.c
@@ -5,50 +5,44 @@
  *      Author: joedo
  */
 
+#include "controller.h"
+#include "freertos.h"
+#include "cmsis_os.h" //TODO replace with xtaskDelayUntil
+
 
 void controlTask(void *argument)
 {
-  /* USER CODE BEGIN controlTask */
-	uint32_t min_controller_frequency = 10; //10 Hz TODO: Make defines for these somewhere more visible
-	uint32_t controller_delay_ticks = 1000 / min_controller_frequency / portTICK_RATE_MS; //equivalent FreeRTOS ticks
-
-	altitudeMessage altitudeEstimate;
-
-	//TODO: Put inside of struct
-	float controller_term_P = 0;
-	float controller_term_I = 0;
-	float controller_term_D = 0;
-	float target_altitude = ALTITUDE_TARGET_FT;
-	float last_error;
-	float error;
-	float target_extension;
+	float altitudeEstimate;
+	controller_t airbrakesController;
 	TickType_t last_ticks = xTaskGetTickCount();
+
 
   /* Infinite loop */
 	for(;;)
 	{
-		//xQueueReceive(altitudeQueue, &altitudeEstimate, 10);
-		altitudeEstimate.altitude = idx*10; //Test code
+		//TODO get altitude message from queue
+		altitudeEstimate = 1000; //Test code
 
-		error = target_altitude - altitudeEstimate.altitude;
-		controller_term_P = error * CONTROLLER_GAIN_P;
+		//PID controller update
+		airbrakesController.error = airbrakesController.target_altitude - altitudeEstimate;
+		airbrakesController.controller_term_P = airbrakesController.error * CONTROLLER_GAIN_P;
 		float dt = (float)(xTaskGetTickCount() - last_ticks) / (portTICK_RATE_MS * 1000); //time delay in ms TODO: convert to more accurate source
-		controller_term_I = controller_term_I + CONTROLLER_GAIN_I * error * dt; //Add some time measure here
-		controller_term_D = (error - last_error) * CONTROLLER_GAIN_D / dt; //here too
+		airbrakesController.controller_term_I = airbrakesController.controller_term_I + CONTROLLER_GAIN_I * airbrakesController.error * dt; //Add some time measure here
+		airbrakesController.controller_term_D = (airbrakesController.error - airbrakesController.last_error) * CONTROLLER_GAIN_D / dt; //here too
 		last_ticks = xTaskGetTickCount();
 
-		float extension = controller_term_P + controller_term_I - controller_term_D;
+		float extension = airbrakesController.controller_term_P + airbrakesController.controller_term_I - airbrakesController.controller_term_D;
 
 		if(extension > CONTROLLER_MAX_EXTENSION) extension = CONTROLLER_MAX_EXTENSION;
 		if(extension < CONTROLLER_MIN_EXTENSION) extension = CONTROLLER_MIN_EXTENSION;
 
-		//xQueueSend(busQueue);
+		//TODO log to queue and send to CAN
 
 		//Test Code
 		char buffer[16] = {0};
-		sprintf(&buffer, "Target Ext: %f", extension);
-		HAL_UART_Transmit(&buffer, art4, &buffer, 16, 10); //Blocking API, 10ms timeout
-		osDelayUntil(controller_delay_ticks); //Implements periodic behaviour (nominal delay - time spent running the loop code)
+		//sprintf(&buffer, "Target Ext: %f", extension);
+		//HAL_UART_Transmit(&buffer, &huart4, &buffer, 16, 10); //Blocking API, 10ms timeout
+		osDelayUntil(CONTROLLER_DELAY_TICKS); //Implements periodic behaviour (nominal delay - time spent running the loop code)
 	}
   /* USER CODE END controlTask */
 }
--- STM32Cube/Tasks/controller.h
+++ STM32Cube/Tasks/controller.h
@@ -15,6 +15,19 @@
 #define CONTROLLER_MAX_EXTENSION 1.0
 #define CONTROLLER_MIN_EXTENSION 0.0
 
+#define MIN_CONTROLLER_FREQUENCY 10
+#define CONTROLLER_DELAY_TICKS 1000 / MIN_CONTROLLER_FREQUENCY / portTICK_RATE_MS
+
+typedef struct {
+	float controller_term_P;
+	float controller_term_I;
+	float controller_term_D;
+	float target_altitude;
+	float last_error;
+	float error;
+	float target_extension;
+} controller_t;
+
 void controlTask(void *argument);
 
 
