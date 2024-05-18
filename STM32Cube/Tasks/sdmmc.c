03e14fc4ba46cba94fbb75303872058cbca87d5b
Joshua Dietrich <59072117+Taggagii@users.noreply.github.com>
2024-05-18 16:42:34 -0400

    functioning version where open works

--- STM32Cube/Tasks/sdmmc.c
+++ STM32Cube/Tasks/sdmmc.c
@@ -1,13 +1,28 @@
 #include "sdmmc.h"
 
+UART_HandleTypeDef* sdmmcHuart;
+
+
+void printLine(const char* message) {
+	HAL_UART_Transmit(sdmmcHuart, (uint8_t *)message, strlen(message), 10);
+	HAL_UART_Transmit(sdmmcHuart, "\n", 1, 10);
+}
+
+
 void sdmmcTask(void *argument) {
+	sdmmcHuart = (UART_HandleTypeDef*)argument;
+
+	printLine("testing things");
+
 	// mount that fucker
 	FATFS fs; // Causes: FR_NOT_READY f_mount (pullup?, how enable?)
 //	FATFS *fs = NULL; // Causes: FR_OK f_mount, FR_NOT_ENABLED f_open
 
-	FRESULT mount_res = f_mount(&fs, "", 1);
+	printLine("mounting");
+	FRESULT mount_res = f_mount(&fs, "", 0);
 
 	if (mount_res != FR_OK) {
+		printLine("FAiled to mount");
 		goto UNMOUNT;
 	}
 
@@ -23,14 +38,18 @@ void sdmmcTask(void *argument) {
 
 //	 create some fuckin file
     FIL file;
+    printLine("Opening file");
     FRESULT open_file_res = f_open(&file, "a.txt", FA_CREATE_ALWAYS);
     if (open_file_res != FR_OK) {
+    	printLine("Failed to open");
         goto UNMOUNT;
     } else {
+    	printLine("Open worked!");
     	f_close(&file);
     }
 
 	UNMOUNT:
+	printLine("Unmounting");
 	f_mount(0, "", 0);
 
 
