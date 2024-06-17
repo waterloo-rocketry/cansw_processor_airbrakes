3ea6b3a244999342096a31fde37911635677563b
celery <71736183+celery6@users.noreply.github.com>
2024-06-16 22:15:14 -0400

    logger improvements

--- STM32Cube/Tasks/log.h
+++ STM32Cube/Tasks/log.h
@@ -16,7 +16,7 @@ extern "C" {
 
 // TODO: determine optimal numbers for these
 /* max length of log data string (bytes) */
-#define MAX_MSG_LENGTH 128
+#define MAX_MSG_LENGTH 256
 /* size of one log buffer (bytes) */
 #define LOG_BUFFER_SIZE 8192
 /* number of log buffers */
@@ -31,21 +31,6 @@ typedef enum {
     LOG_LVL_INFO = 'I',   // Info (data from sensors, etc)
     LOG_LVL_DEBUG = 'D',  // Only for debugging on the ground
 } LogLevel_t;
-
-/**
- * Log data source
-*/
-typedef enum
-{
-    SOURCE_FLIGHT_EVENT,
-    SOURCE_HEALTH,
-    SOURCE_CAN_RX,
-    SOURCE_CAN_TX,
-    SOURCE_SENSOR,
-    SOURCE_STATE_EST,
-    SOURCE_APOGEE_PRED,
-    SOURCE_EXT_TARGET    
-} LogDataSource_t;
 
 /**
  * Buffer holding one block of log msgs
@@ -64,17 +49,17 @@ bool logInit(void);
 /**
  * Log an error-level message
 */
-bool logError(const LogDataSource_t source, const char* msg, ...);
+bool logError(const char* source, const char* msg, ...);
 
 /**
  * Log an info-level message
 */
-bool logInfo(const LogDataSource_t source, const char* msg, ...);
+bool logInfo(const char* source, const char* msg, ...);
 
 /**
  * Log a debug-level message
 */
-bool logDebug(const LogDataSource_t source, const char* msg, ...);
+bool logDebug(const char* source, const char* msg, ...);
 
 /**
  * FreeRTOS task for the logger
