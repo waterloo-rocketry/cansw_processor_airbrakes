a42a14c021dc7de8eac5ddb02015119c0a3d85aa
Christine Li <71736183+celery6@users.noreply.github.com>
2024-09-30 20:44:20 -0400

    fix gpio pin

--- STM32Cube/Tasks/blinky.c
+++ STM32Cube/Tasks/blinky.c
@@ -17,15 +17,15 @@ void blinkyTask(void *argument) {
 	for (;;) {
 		// Write 0 to the LED pin (turn it off)
 		// HAL functions are provided by STM32 to easily interact with hardware
-		// LEDs are wired to the STM32's GPIO pins - this one specifically is wired to GPIO_B pin 5
-		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, 0);
+		// LEDs are wired to the STM32's GPIO pins - this one specifically is wired to GPIO_E pin 10
+		HAL_GPIO_WritePin(GPIOE, GPIO_PIN_10, 0);
 
 		// FreeRTOS function that puts this task on pause for 500 ticks (0.5 sec)
 		// Other tasks will run while this task is paused
 		vTaskDelay(500);
 
 		// Write 1 to the LED pin (turn it on)
-		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, 1);
+		HAL_GPIO_WritePin(GPIOE, GPIO_PIN_10, 1);
 
 		// Pause task for 500 ticks (0.5 sec)
 		vTaskDelay(500);
